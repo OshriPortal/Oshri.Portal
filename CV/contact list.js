dfc76db51fb65949dce271d78d7d2076cbc8f578
OshriPortal <portalo@post.bgu.ac.il>
2021-05-08 09:48:20 +0300

    Assignment7

---
+++ CV/contact list.js
@@ -0,0 +1,28 @@
+
+
+fetch('https://reqres.in/api/users?page=2').then(
+    response => response.json()
+)
+    .then(
+        responseJSON => createUsersList(responseJSON.data)
+    )
+    .catch(
+        err => console.log(err)
+    )
+
+function createUsersList(users){
+    const curr_main = document.querySelector("main");
+       for(let user of users){
+        const section = document.createElement('section');
+        section.innerHTML = `
+        <img src="${user.avatar}" alt="Profile Picture"/>
+        <div>
+        <span>${user.first_name} ${user.last_name}</span>
+        <br>
+        <a href="mailto:${user.email}">Send Email</a>
+        </div>
+        <br><br>
+        `;
+        curr_main.appendChild(section);
+         }
+}
