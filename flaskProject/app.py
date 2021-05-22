9cf3f3f19b73c9187bd7d0f0b4730f22537ca73f
OshriPortal <portalo@post.bgu.ac.il>
2021-05-22 18:11:19 +0300

    Ass8

--- flaskProject/app.py
+++ flaskProject/app.py
@@ -1,22 +1,40 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def bgu():
-    return redirect("https://in.bgu.ac.il/Pages/default.aspx")
-
-
-@app.route('/')
-def assignment7():
-    return 'Assignment 7- flask'
-
-
-@app.route('/assignment7')
+@app.route('/main', methods=['PUT', 'POST', 'GET'])
+@app.route('/', methods=['PUT', 'POST', 'GET'])
 def main():
-    return redirect(url_for('assignment7'))
+    return render_template('cv.html')
+
+
+@app.route('/contact%20list.html')
+def contact():
+    return render_template('contact list.html')
+
+
+@app.route('/assignment8', methods=['PUT', 'POST', 'GET'])
+def assignment8():
+    contact_user = {'firstname': 'Michael', 'lastname': 'Lawson'}
+    hobbies = ['Scuba diving', 'football', 'movies']
+    divingSites = ['Red sea', 'Mexico', 'Galapagos']
+    movies = ['fight club', 'the godfather', 'the dark knight']
+    footballClubs = ['FC Ashdod', 'FC Barcelona']
+    return render_template('assignment8.html',
+                           hobbies=hobbies,
+                           divingSites=divingSites,
+                           movies=movies,
+                           footballClubs=footballClubs,
+                           contact_user=contact_user)
+
+
+@app.route('/games', methods=['PUT', 'POST', 'GET'])
+def games():
+    games = ['FIFA', 'billiard', 'Beer Pong']
+    return render_template('games.html',
+                           games=games)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
